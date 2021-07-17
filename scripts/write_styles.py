bd307f78aa7c479246a7f9003eb0195e732dd0d2
Mark Vasilkov <mvasilkov@gmail.com>
2021-07-18 00:49:46 +0300

    Support for the NL variant of the font

--- scripts/write_styles.py
+++ scripts/write_styles.py
@@ -35,12 +35,12 @@ fonts = {
 
 scss = '''
 @font-face {{
-  font-family: 'JetBrains Mono';
+  font-family: 'JetBrains Mono{nl_space}';
   font-style: {normal_italic};
   font-weight: {weight};
-  src: local('JetBrains Mono{suffix_space}'),
-    local('JetBrainsMono-{suffix}'),
-    url('{font_dir}/JetBrainsMono-{suffix}.woff2') format('woff2');
+  src: local('JetBrains Mono{nl_space}{suffix_space}'),
+    local('JetBrainsMono{nl}-{suffix}'),
+    url('{font_dir}/JetBrainsMono{nl}-{suffix}.woff2') format('woff2');
   font-display: swap;
 }}
 '''
@@ -54,8 +54,8 @@ test_html = '''
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>JetBrains Mono test</title>
-        <link rel="stylesheet" href="../css/jetbrains-mono.css">
+        <title>JetBrains Mono{nl_space} test</title>
+        <link rel="stylesheet" href="../css/{css_file}">
     </head>
     <body>
 {test_cases}
@@ -64,8 +64,8 @@ test_html = '''
 '''
 
 test_case = '''
-<p style="font-family: 'JetBrains Mono'; font-style: {normal_italic}; font-weight: {weight};">
-    JetBrains Mono test string (style={normal_italic} weight={weight})
+<p style="font-family: 'JetBrains Mono{nl_space}'; font-style: {normal_italic}; font-weight: {weight};">
+    JetBrains Mono{nl_space} test string =&gt; (style={normal_italic} weight={weight})
 </p>
 '''
 
@@ -74,7 +74,10 @@ def get_suffix_space(font):
     return '' if font == 'Regular' else ' ' + font
 
 
-def run():
+def run(is_nl=False):
+    nl = 'NL' if is_nl else ''
+    nl_space = ' NL' if is_nl else ''
+
     result = font_dir_init
     tests = ''
 
@@ -84,33 +87,39 @@ def run():
             font_dir=font_dir,
             normal_italic='normal',
             weight=props['Weight'],
+            nl=nl,
+            nl_space=nl_space,
             suffix=font,
-            suffix_space=' ' + font  # get_suffix_space(font)
+            suffix_space=' ' + font,  # get_suffix_space(font)
         )
         # font: italic
         result += scss.format(
             font_dir=font_dir,
             normal_italic='italic',
             weight=props['Weight'],
+            nl=nl,
+            nl_space=nl_space,
             suffix=props['Italic'],
-            suffix_space=get_suffix_space(font) + ' Italic'
+            suffix_space=get_suffix_space(font) + ' Italic',
         )
 
         # test: normal
-        tests += test_case.format(normal_italic='normal', weight=props['Weight'])
+        tests += test_case.format(normal_italic='normal', weight=props['Weight'], nl_space=nl_space)
         # test: italic
-        tests += test_case.format(normal_italic='italic', weight=props['Weight'])
+        tests += test_case.format(normal_italic='italic', weight=props['Weight'], nl_space=nl_space)
 
     project_root = Path(__file__).parents[1]
 
-    out_file = project_root / 'scss' / 'jetbrains-mono.scss'
+    out_file = project_root / 'scss' / ('jetbrains-mono-nl.scss' if is_nl else 'jetbrains-mono.scss')
     out_file.write_text(result, encoding='utf-8')
 
-    test_result = test_html.format(test_cases=tests).lstrip()
+    css_file = 'jetbrains-mono-nl.css' if is_nl else 'jetbrains-mono.css'
+    test_result = test_html.format(css_file=css_file, nl_space=nl_space, test_cases=tests).lstrip()
 
-    test_file = project_root / 'test' / 'index.html'
+    test_file = project_root / 'test' / ('nl.html' if is_nl else 'index.html')
     test_file.write_text(test_result, encoding='utf-8')
 
 
 if __name__ == '__main__':
     run()
+    run(is_nl=True)
