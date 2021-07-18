cbae4abff2ea9c933ad937f0bf4e3bb942807fff
Mark Vasilkov <mvasilkov@gmail.com>
2021-07-18 10:22:08 +0300

    The variant of the font without ligatures
    Closes #4
    Closes #5

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -4,7 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# clean
+DIRS='css scss test'
+rm -rf $DIRS
+mkdir -p $DIRS
+
+# build
 ./sources/build-webfonts.sh
 ./scripts/write_styles.py
-node-sass -o css scss/jetbrains-mono.scss
-node-sass -o css scss/jetbrains-mono-nl.scss
+./node_modules/.bin/node-sass -o css scss/jetbrains-mono.scss
+./node_modules/.bin/node-sass -o css scss/jetbrains-mono-nl.scss
